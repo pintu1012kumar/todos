125878e40709c3a4c1a9df6662aecf8677f1a9d8
PINTU KUMAR <pintu1012kumar@gmail.com>
2025-09-08 18:10:51 +0530

    update file-services

--- src/app/documents/file-service.ts
+++ src/app/documents/file-service.ts
@@ -359,6 +359,9 @@ export const deleteUserFile = async (userId: string, fileName: string) => {
   if (error) throw error;
 };
 
+
+
+//get
 export const getPublicUrlForUserFile = (
   userId: string,
   fileName: string
